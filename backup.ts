7e663d40c14a9248a936d335676f2ba74f7b1303
Nauris Pūķis <hi@pyronaur.com>
2024-04-21 23:05:05 +0300

    add backup

--- backup.ts
+++ backup.ts
@@ -1,30 +1,75 @@
 /**
- * Backup a remote WordPress database.
- * @usage backup <domain.com> <username@hostname> <backup_path>
+ * Backup a WordPress site
  */
-
-import { $remote, getSiteInfo } from './_utils';
-import { getDatabase } from './pull';
-
-
-export default async function () {
-	const { DOMAIN, REMOTE, PATH_REMOTE } = await getSiteInfo();
-	if (!args[2]) {
-		throw new Error("Missing required arguments. Usage: backup <domain.com> <username@hostname> <backup_path>");
-	}
-
-	const BACKUP_PATH = args[2];
-
-	console.log(`===\nBacking up database for ${DOMAIN}...\n===`);
-	await getDatabase(REMOTE, PATH_REMOTE, '/tmp');
-	const backupDirExists = await isDirectory(BACKUP_PATH);
-	if (!backupDirExists) {
-		console.log(`Creating backup directory at ${BACKUP_PATH}`);
-		await $`mkdir -p ${BACKUP_PATH}`;
-	}
-	const backupFileName = `${new Date().toISOString().split('T')[0]}-${DOMAIN}.tar.gz`;
-	await $`mv /tmp/db.tar.gz ${BACKUP_PATH}/${backupFileName}`;
-	console.log(ansis.green(`Database backup complete. Stored at ${BACKUP_PATH}/${backupFileName}`));
+import { getConfig } from './lib/config';
+import { $remote, $remoteFileExists, rsync } from './lib/utils';
+import { unlinkSync } from "node:fs";
+function formatDate(date: Date): string {
+	const year = date.getFullYear();
+	const month = String(date.getMonth() + 1).padStart(2, '0');
+	const day = String(date.getDate()).padStart(2, '0');
+	return `${year}-${month}-${day}`;
 }
 
+async function verifyTarIntegrity(tarFile: string): Promise<void> {
+	try {
+		await $`tar -tvf ${tarFile}`;
+		console.log(`Verified the integrity of ${tarFile}`);
+	} catch (error) {
+		throw new Error(`Failed to verify the integrity of ${tarFile}`);
+	}
+}
+
+export default async function backup() {
+	const config = await getConfig(await cwd());
+	const { ssh_host, path, backup } = config;
+	const { remote: remotePath } = path;
+	const backupDestination = backup.destination.trim();
+
+	const dateString = formatDate(new Date());
+	const tmpName = `${config.domain.remote}-${dateString}`;
+	const tmpDir = `/tmp/${tmpName}`;
+	const filesDir = `${tmpDir}/files`;
+	const dbFile = `${tmpDir}/db.sql`;
+	const tarFile = `${tmpDir}.tar.gz`;
+
+	// 1. Copy the site files to /tmp/site-{YYYY-MM-DD}/files
+	if (await $remoteFileExists(ssh_host, filesDir, 'd') !== true) {
+		await $remote(ssh_host, `mkdir -p ${filesDir} && cp -R ${remotePath}/ ${filesDir}`);
+	} else {
+		console.log('Site files already copied. Skipping step 1.');
+	}
+
+	// 2. Create a db.sql file in /tmp/site-{YYYY-MM-DD}/
+	if (await $remoteFileExists(ssh_host, dbFile, 'f') !== true) {
+		await $remote(ssh_host, `wp db export --path=${remotePath} ${dbFile}`);
+	} else {
+		console.log('Database export already exists. Skipping step 2.');
+	}
+
+	// 3. Create a tar.gz file out of the /tmp/site-{YYYY-MM-DD} directory
+	if (await $remoteFileExists(ssh_host, tarFile, 'f') !== true) {
+		await $remote(ssh_host, `tar -czf ${tarFile} -C ${tmpDir} .`);
+	} else {
+		console.log('Tar file already exists. Skipping step 3.');
+	}
+
+	// 4. Rsync the tar.gz file to the backup directory
+	const localFile = `${backupDestination}/${tmpName}.tar.gz`
+	if (await Bun.file(localFile).exists() === false) {
+		await $`rsync --progress --human-readable ${ssh_host}:${tarFile} ${localFile}`
+	} else {
+		console.log(`'${localFile}' already exists. Skipping step 4.`);
+	}
+
+	// Verify the integrity of the downloaded tar.gz file
+	await $spinner(`Verifying the integrity of ${tarFile}`, () =>
+		verifyTarIntegrity(tarFile)
+	);
+
+	// 5. Remove the temporary files and directory
+	await $remote(ssh_host, `rm -rf ${tmpDir} ${tarFile}`);
+	console.log('Backup completed successfully!');
+	console.log(`View Backup Directory: ${backupDestination}`);
+}
 
--- lib/utils.ts
+++ lib/utils.ts
@@ -4,6 +4,15 @@ export async function $remote(host: string, command: string) {
 	return $spinner(($) => $`ssh ${host} '${{ raw: command }}'`);
 }
 
+export async function $remoteFileExists(host: string, file: string, type: 'f' | 'd') {
+	try {
+		await $`ssh ${host} 'test -${type} ${file}'`.quiet();
+		return true;
+	} catch (error) {
+		return false;
+	}
+}
+
 export async function rsync(from: string, to: string, additionalFlags: string = '') {
-	return $`rsync -az --delete --progress --human-readable ${from} ${to} ${{ raw: additionalFlags }}`;
+	return $`rsync -avz --delete --progress --human-readable ${from} ${to} ${{ raw: additionalFlags }}`;
 }
